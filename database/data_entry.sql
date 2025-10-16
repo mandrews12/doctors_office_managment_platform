5dc3f1fb5c3b458dd6fb6093add23709ca60e98a
mandrews12 <ma17328ma@uri.edu>
2025-10-16 13:10:11 -0400

    Add SQL scripts for data insertion and query fetching

---
+++ database/data_entry.sql
@@ -0,0 +1,15 @@
+-- insert data into patient table
+
+-- insert data into appointment table
+
+-- insert data into doctor table
+
+-- insert data into past_visits table
+
+-- insert data into staff table
+
+-- insert data into supplies table
+
+-- insert data into staff table
+
+-- insert data into insurance table
---
+++ database/queries.sql
@@ -0,0 +1,26 @@
+
+-- query to fetch all patients
+
+-- query to fetch all doctors
+
+-- query to fetch all appointments
+
+-- query to fetch appointments for a specific patient
+
+-- query to fetch appointments for a specific doctor
+
+-- query to fetch patients assigned to a specific doctor
+
+-- query to fetch past visits for a specific patient
+
+-- quert to fetch staff details
+
+-- query to fetch supplies details
+
+-- query to fetch supplies provided by a specific staff member
+
+-- query to fetch doctors with a specific role
+
+-- query to fetch patients with a specific ailment
+
+-- query to fetch appointments with a specific status
